961448827601fafe272344ad9dbd5d2343aa02ff
nghiand <nghiand@ominext.com>
2020-06-17 15:59:43 +0700

    update

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Nghia kaka
+          Learn React Nghia
         </a>
       </header>
     </div>
